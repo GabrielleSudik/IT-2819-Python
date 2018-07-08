c3c44f7e1df34db4ccbcf8a16900e46101d38a3d
GabrielleSudik <gabrielle.b.sudik@gmail.com>
2018-07-08 18:15:05 -0400

    note through lesson 10

--- IT2819 lesson 10 notes and follow-along.py
+++ IT2819 lesson 10 notes and follow-along.py
@@ -1,2 +1,51 @@
 # lesson 10 follow-along
+# global and local scope
 
+spam = 42 #global scope. forgotten when the program ends.
+
+def eggs():
+    spam = 43 #local scope.
+    #local variables are forgotten when function returns its value.
+
+print('Some code')
+print('Some more code')
+
+# code in global scope can't use local variables.
+# code in local can use global variables.
+# one local scope variable can't be used in a different local scope.
+# the same name can represent different variables if in different scopes.
+
+def spam():
+    eggs = 99
+    bacon()
+    print(eggs)
+
+def bacon():
+    ham = 101
+    eggs = 0
+
+spam() #returns "eggs" which is 99 in spam.
+       #even though eggs was 0 within bacon, that variable was forgotten
+       #when we left the bacon function
+
+def spam():
+    #eggs = 30
+    print(eggs)
+
+eggs = 40
+spam()  #returns 40 because spam uses a global variable.
+        #if you uncode the eggs inside of spam(), you'll get 30 instead.
+
+#can you assign a global variable within a local scope? yes"
+def spam():
+    global eggs
+    eggs = 20
+    print(eggs)
+
+eggs = 50
+spam() #prints 20, which is from eggs inside spam
+print(eggs) #prints 20 because you re-assigned global eggs to 20 within spam
+            #even though you declared eggs was 50 in the global scope
+            #and spam reassigned eggs AFTER you assigned the global as 50
+eggs = 60
+print(eggs) #prints 60, because it was once again reassigned globally.
--- IT2819 lesson reading and writing files follow-along.py
+++ IT2819 lesson reading and writing files follow-along.py
@@ -1,4 +1,5 @@
 # Random tutorial about reading and writing text files.
+# Watched for assignment 10.
 # Start with a txt file created in the same folder as this py file.
 
 # this next thing is a context manager. it's considered best practice.
@@ -21,15 +22,69 @@ with open('LessonText.txt', 'r') as f:
     print(f_contents)
     f_contents = f.read(10) #prints next 10 characters of f
     print(f_contents)
+
+print()
+
+#way when you don't know length of doc:
+with open('LessonText.txt', 'r') as f:
+    size_to_read = 10
+    f_contents = f.read(size_to_read)
+    while len(f_contents) > 0:
+        print(f_contents, end='*')
+        f_contents = f.read(size_to_read)
     
 
 #old way of doing it:
 fi = open('LessonText.txt', 'r')
 # r means we can read. w means write. r+ means read and write.
 
+print()
 print(fi.name)
 print(fi.mode) # prints how we are viewing it (r)
 
 fi.close()
 # if you don't close the file, you can get leaks then errors.
 
+# there are more methods under read. Like "tell" says where you are.
+
+# writing!
+# careful you don't overwrite a file you don't mean to.
+
+# to create a new file:
+with open('LessonText2.txt', 'w') as f:
+    pass
+
+# to write something to it:
+with open('LessonText2.txt', 'w') as f:
+    f.write('Test text')
+
+# fyi here's an example of a more robust IDE being useful.
+# you can keep check the text files within the IDE's tabs
+
+with open('LessonText2.txt', 'w') as f:
+    f.write('Second line')
+    f.write('Third line')
+
+# If you ran those last two blocks, you'll see that the second block overwrote the first
+# multiple write lines in the same block will just keep printing
+
+# copying a file:
+with open('LessonText.txt', 'r') as rf:
+    with open('LessonTextCopy.txt', 'w') as wf:
+        for line in rf:
+            wf.write(line)
+            
+# copying a picture file:
+with open('Gabrielle.png', 'rb') as rf:
+    with open('GabrielleCopy.png', 'wb') as wf:
+        for line in rf:
+            wf.write(line)
+
+# or:
+with open('Gabrielle.png', 'rb') as rf:
+    with open('GabrielleSecondCopy.png', 'wb') as wf:
+        chunk_size = 128 # that number denotes size of chunk. could be different.
+        rf_chunk = rf.read(chunk_size)
+        while len(rf_chunk) > 0:
+            wf.write(rf_chunk)
+            rf_chunk = rf.read(chunk_size)
