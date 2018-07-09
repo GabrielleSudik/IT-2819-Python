159d4e3c41fbacbc8126e38b409af5c3b3cb2833
GabrielleSudik <gabrielle.b.sudik@gmail.com>
2018-07-08 21:26:04 -0400

    Assignments 7 and 8 done

--- A07-Sudik.py
+++ A07-Sudik.py
@@ -2,25 +2,40 @@
 # Your mission is to use a for loop to check to see if the number entered
 # by the use is actually a prime number.
 
+import math
+
 print("Assignment 07 - written by Gabrielle Sudik")
 # get user input
 num = input("Enter a number: ")
 while num.upper()!= "X":
  # check to make sure we have an integer, if not, quit
  if num.isdigit() != True:
- print("A number must be entered.")
- quit()
+     print("A number greater than 1 must be entered. Goodbye!")
+     quit()
  # convert the imput string to an integer
  numInt = int(num)
  # prime numbers are greater than 1
  if numInt > 1:
- print("Check for factors.")
- # code for prime number generation goes here:
+     print("Check for factors.")
+     # code for prime number generation goes here:
+     start = 2
+     if numInt == 2:
+         print(numInt, "is a prime number.")
+     if numInt > 2:
+         for i in range(start, numInt):
+             if numInt % i == 0:
+                print(numInt, "is not a prime number.")
+                break
+             else:
+                print(numInt, "is a prime number.")
+                start = numInt+1
+                break
 
  # if input number is less than
  # or equal to 1, it is not prime
  else:
- print(numInt,"is not a prime number")
+     print(numInt, "is not a prime number.")
  # get user input
- num = input("Enter a number: ")
-print("Goodbye!")
+ num = input("Enter another number: ")
+print("Goodbye!")
+
--- A08-Sudik.py
+++ A08-Sudik.py
@@ -1,1 +1,35 @@
 #reading & writing files
+#1. open the files
+#2. read each line from the input file
+#3. print each line to the command prompt
+#4. and then write the line to the output file
+
+#open file and print each line with a for loop:
+with open('A06input.txt', 'r') as f:
+    for line in f:
+        print(line)
+
+#create a new file:
+with open('A06output.txt', 'w') as f:
+    pass
+
+#print stuff to new file
+#name source file
+with open('A06input.txt', 'r') as rf:
+    #name destination file
+    with open('A06output.txt', 'w') as wf:
+        #write one line to tell readers what it is
+        wf.write('This is the output text file.\n')
+        #for loop to print each line in source file to output file
+        for line in rf:
+            wf.write(line)
+
+tryingSomething = ['apple', 'banana', 'carrot']
+
+with open('A06output.txt', 'a') as wf: #for append, so you don't overwrite
+    for fruit in tryingSomething:
+        wf.write(fruit)
+        wf.write('\n')
+
+#that also did a for loop to print stuff, but stuff created within the code
+#instead of an outside file.
