62bf77b85d7daad888bdbd9763fb25ec3d5bf0dd
GabrielleSudik <gabrielle.b.sudik@gmail.com>
2018-08-05 22:46:49 -0400

    coursework complete, folder cleaned up

--- IT2819 lesson 1 notes and follow-along.py
+++ IT2819 lesson 1 notes and follow-along.py
@@ -1,1 +1,12 @@
 #Python Lesson 1 Notes
+
+#Python -- simple and gentle. Good intro to coding.
+#All you need is a Python interpreter.
+
+print('Hello, World!') #prints that.
+
+#For best learning, type along with lessons.
+
+#Prepare to google all the time! Copy and pasting error messages is a start.
+#If you ask questions (esp online) give as much detail as possible.
+#Include what you've tried and related info. And versions of everything.
